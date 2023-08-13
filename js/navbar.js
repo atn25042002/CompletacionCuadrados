49ffe7ea51407e386111ed1f5e5e97a65f61e5cb
ant25042002 <Aacot@unsa.edu.pe>
2023-08-13 12:10:18 -0500

    generalizando la chargePage

--- js/navbar.js
+++ js/navbar.js
@@ -3,3 +3,9 @@ function helpPage(){
     console.log("Pagina Ayuda");
     miDiv.innerHTML= '<iframe src="./pages/help.html" width="800" height="600"></iframe>';
 }
+
+function chargePage(ent){
+    var miDiv = document.getElementById("espacio");
+    console.log("Pagina Ayuda");
+    miDiv.innerHTML= '<iframe src="./pages/' + ent +'.html" width="800" height="600"></iframe>';
+}

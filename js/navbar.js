9ba4b8b4544637750d003c6fbab10c18200362f0
ant25042002 <Aacot@unsa.edu.pe>
2023-08-13 12:05:01 -0500

    agregando estilos y cargador de pagina

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+function helpPage(){
+    var miDiv = document.getElementById("espacio");
+    console.log("Pagina Ayuda");
+    miDiv.innerHTML= '<iframe src="./pages/help.html" width="800" height="600"></iframe>';
+}

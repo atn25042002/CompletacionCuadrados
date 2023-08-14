2e6572158011625cb8c0aff3f60699bafe60bef1
ant25042002 <Aacot@unsa.edu.pe>
2023-08-13 22:11:01 -0500

    Corrigiendo formato y decimales

--- js/raices.js
+++ js/raices.js
@@ -4,45 +4,48 @@ function calcular(){
     let c= document.getElementById("coeficienteC").value;
     let lista= document.getElementById("lista");
     lista.innerHTML= "<ul id='lista'></ul>";
+    lista.appendChild(create("h3","RESOLUCIÓN:"))
     lista.appendChild(create("li","La ecuación cuadrática es: "));
     lista.appendChild(create("p",`${a}x² + ${b}x + ${c} = 0`))
     if(a != 1){
         lista.appendChild(create("li","Dividimos entre en primer coeficiente:"));
         lista.appendChild(create("p",`x² + (${b}/${a})x + (${c}/${a}) = 0`));
-        b= b/a;
-        c= c/a;
+        b= (b/a).toFixed(6);
+        c= (c/a).toFixed(6);
         lista.appendChild(create("p",`x² + (${b})x + (${c}) = 0`));
     }
     lista.appendChild(create("li","Completamos cuadrados"));
     lista.appendChild(create("p",`x² + 2(${b}/2)x + (${b}/2)² + (${c}) = (${b}/2)²`));
-    lista.appendChild(create("p",`x² + 2(${b/2})x + (${b/2})² = (${b/2})² - (${c})`));
-    let dis= b*b/4-c;
-    lista.appendChild(create("p",`(x + ${b/2})² = ${dis}`));
+    b=b/2;
+    lista.appendChild(create("p",`x² + 2(${b})x + (${b})² = (${b})² - (${c})`));
+    let dis= (Math.pow(b,2)-c).toFixed(4);
+    lista.appendChild(create("p",`(x + ${b})² = ${dis}`));
     let i ="";
     if(dis < 0){
         i= "i";
         dis = -1*dis;
     }
     lista.appendChild(create("li","Despejamos las raices"));
-    lista.appendChild(create("p",`x + ${b/2} = ± √(${dis})${i}`));
-    lista.appendChild(create("p",`x = - ${b/2} ± √(${dis})${i}`));
+    lista.appendChild(create("p",`x + ${b} = ± √(${dis})${i}`));
+    lista.appendChild(create("p",`x = - ${b} ± √(${dis})${i}`));
     lista.appendChild(create("li","Raices halladas: "));
+    let sum= Math.sqrt(dis).toFixed(6);
     if(i == "i"){
-        lista.appendChild(create("p",`x\u2081 = - ${b/2} + √(${dis})${i}`));
-        lista.appendChild(create("p",`x\u2082 = - ${b/2} - √(${dis})${i}`));
+        lista.appendChild(create("p",`x\u2081 = - ${b} + ${sum} ${i}`));
+        lista.appendChild(create("p",`x\u2082 = - ${b} - ${sum} ${i}`));
     }else if(dis == 0){
-        lista.appendChild(create("p",`x\u2081 = ${b/2}`));
+        lista.appendChild(create("p",`x\u2081 = ${b}`));
     }
     else{
-        lista.appendChild(create("p",`x\u2081 = ${-b/2 + Math.sqrt(dis)}`));
-        lista.appendChild(create("p",`x\u2082 = ${-b/2 - Math.sqrt(dis)}`));
+        lista.appendChild(create("p",`x\u2081 = ${-b + parseFloat(sum)}`));
+        lista.appendChild(create("p",`x\u2082 = ${-b - sum}`));
     }
 }
 
 function create(ent, cont){
     let element= document.createElement(ent);
     element.classList.add("paso");
-    element.textContent=cont.replace(/1x/g,"x");
+    element.textContent=cont.replace(/1x/g,"x").replace(/\+ -/g,"- ");
     return element;
 }
 
